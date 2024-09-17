6735ece48aa7cfea7300ed13d21e761cdd11c468
“TaifAldehbashgit config --global user.email “taifmaldehbash@gmail.com <77803844+TaifAldehbash@users.noreply.github.com>
2024-09-17 16:30:38 +0300

    Initial Commit

---
+++ DailyMoods/DailyMoodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyMoodsApp.swift
+//  DailyMoods
+//
+//  Created by Taif Al.qahtani on 17/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct DailyMoodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
